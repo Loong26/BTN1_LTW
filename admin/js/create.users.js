8f77565d1fda95c4c8191bef95fdbf081f626bc0
Nguyễn Đức Anh <ducanhnguyend21cntt@gmail.com>
2024-03-05 09:56:58 +0700

    Update create.users.js

--- admin/js/create.users.js
+++ admin/js/create.users.js
@@ -173,3 +173,18 @@ if(buttonCreate) {
   });
 }
 // end submit
+
+// upload image 
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if(file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+// end upload image 
